e942149e79086c092e673b6e6616b0d71e176540
MadhuSudhanGowda97 <127107178+MadhuSudhanGowda97@users.noreply.github.com>
2023-04-25 13:21:33 +0530

    Add files via upload

--- druid/api.js
+++ druid/api.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const request1 = require('request');
 const express = require('express');
 const app = express();
 const port = 2025;
@@ -6,62 +6,74 @@ const druidUrl = 'http://localhost:8888/druid/indexer/v1/supervisor';
 const sqlqueryUrl = 'http://localhost:8888/druid/v2/sql';
 const nativequeryUrl = 'http://localhost:8888/druid/v2/?pretty';
 
- app.use(express.json());
+app.use(express.json());
 
+
+  
 app.post('/createdatasource', (req, res) => {
-    try{
-        const ingestionSpec = req.body;
-  request.post({
+  const ingestionSpec = req.body;
+  request1.post({
     url: druidUrl,
     headers: {'content-type': 'application/json'},
     body: JSON.stringify(ingestionSpec)
   }, (error, response) => {
+    if (error) {
+      res.status(500).send(error);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      res.status(400).send(response);
+      return;
+    }
     res.status(200).send(response);
   });
-    }
-    catch(error){
-        res.send(error)
-    }
-  
 });
 
-app.post('/query', (req, res) => {
-    try{
-        const query = req.body;
-  request.post({
-    url: sqlqueryUrl,
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify(query)
-  }, (error, response, body) => {
-      const parsedBody = JSON.parse(body);
-      res.status(200).send(parsedBody);
-    })
-    }
 
-   catch (error) {
-      res.send(error);
+
+app.post('/query', (req, res) => {
+  const query = req.body;
+  request1.post({
+    url: sqlqueryUrl,
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(query)
+  }, (error, response) => {
+    try{
+      const parsedBody = JSON.parse(response.body);
+    res.status(200).send(parsedBody);
     }
-  });
+    catch{
+      res.status(500).send(error)
+    }
+    
+  }
+  )
+
+});
+
 
 app.post('/nativequery', (req, res) => {
-    try {
     const nquery = req.body;
-  request.post({
-    url: nativequeryUrl,
-    headers: {'content-type': 'application/json'},
-    body: JSON.stringify(nquery)
-  }, (error, response, body) => {
-      const parsedBody = JSON.parse(body);
+    request1.post({
+      url: nativequeryUrl,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(nquery)
+    }, (error, response) => {
+      try{
+        const parsedBody = JSON.parse(response.body);
       res.status(200).send(parsedBody);
+      }
+      catch{
+        res.status(500).send(error)
+      }
+      
     }
-)}
-     catch (error) {
-      res.send(error);
-    }
-  
+    )
+
 });
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
 
+module.exports = { app, request1 };
